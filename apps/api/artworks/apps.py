1ec29e641efc1de0aae00c0e1b27c5325d8abee0
ktn-andrea <ktn.andrea@gmail.com>
2026-09-05 23:37:39 +0200

    Set up Ruff for Python linting and formatting

--- apps/api/artworks/apps.py
+++ apps/api/artworks/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ArtworksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'artworks'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "artworks"
